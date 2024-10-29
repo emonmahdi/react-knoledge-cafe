20695c5816c46e447393852cc57cb489c3ce87ac
emonmahdi <developeremon1997@gmail.com>
2024-10-29 17:06:08 +0600

    show blog data

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -1,8 +1,35 @@
+/* eslint-disable react/prop-types */
 import PropTypes from "prop-types";
 
 export const Blog = ({ blog }) => {
+  const {
+    title,
+    cover,
+    author_name,
+    author_img,
+    posted_date,
+    reading_time,
+    hashtags,
+  } = blog;
   console.log(blog);
-  return <div>Blog</div>;
+  return (
+    <div className="mb-12">
+      <img src={cover} alt="" />
+      <h3>{title}</h3>
+      <div className="flex justify-between items-center">
+        <div className="flex gap-3">
+          <img src={author_img} width={"50px"} alt="" />
+          <div>
+            <h3>{author_name}</h3>
+            <p>{posted_date}</p>
+          </div>
+        </div>
+        <div>
+          <span>{reading_time} min read</span>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 Blog.prototype = {
--- src/components/Blogs/Blogs.jsx
+++ src/components/Blogs/Blogs.jsx
@@ -12,10 +12,12 @@ export const Blogs = () => {
       });
   }, []);
 
-  return <div className="md:w-2/3">
-    <h2>Blogs {blogs.legnth}</h2>
-    {
-      blogs.map((blog) => <Blog key={blog.id} blog={blog} /> )
-    }
-  </div>;
+  return (
+    <div className="md:w-2/3 p-16">
+      <h2>Blogs {blogs.legnth}</h2>
+      {blogs.map((blog) => (
+        <Blog key={blog.id} blog={blog} />
+      ))}
+    </div>
+  );
 };
